69aa0d839bb25d844d069b7f366ff68e336dd075
Martin <noways2170@gmail.com>
2020-03-10 15:06:31 +1100

    created login/password has for terminal apph

--- terminal_app.rb
+++ terminal_app.rb
@@ -1,1 +1,20 @@
-first line 
+login_password = {
+    "Martin" => "don't worry about it",
+    "Morgan" => "it's me",
+    "Andy" => "no need to ask"
+}
+
+puts "What is your login?"
+login = gets.chomp 
+if login_password.has_key?(login)
+    puts "What is your password?"
+
+   
+    password = gets.chomp
+if login_password[login] == password 
+    puts "welcome" 
+else 
+    puts "WHO ARE YOU"
+end 
+end 
+     
